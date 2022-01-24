16db5dec1a93afaf88cc50cd81369ee45ed574b6
bitfl0wer <fleeprivate@gmail.com>
2022-01-24 17:58:23 +0100

    Bigger backup

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ parsed_input = []
 
 def iterative(data):
     iter_output = []
-    curr_iter = 0
     curr_file = ''
     for data_iter in data:
-        print('iter: current iter ', data_iter)
-        print('iter: data_iter[1] = ', data_iter[1])
+        # print('iter: current iter ', data_iter)
+        # print('iter: data_iter[1] = ', data_iter[1])
         if data_iter[1] == 'file' and data_iter[0] == 'START':
-            print('iter: encountered file')
+            # print('iter: encountered file')
             curr_file = data_iter[2]
         if data_iter[1] == 'dependency':
-            print('iter: encountered dependency')
-            print('iter: curr_file: ', curr_file)
-            iter_output.append([curr_file[1], 'a dependency'])
+            # print('iter: encountered dependency')
+            # print('iter: curr_file: ', curr_file)
+            iter_output.append([curr_file[1], data_iter[2][1]])
     return iter_output
 
 
@@ -28,17 +27,27 @@ class HTMLToAsciiDoc(HTMLParser):
             children = attr
         parsed_input.append(["START", tag, children])
 
-    def handle_endtag(self, tag):
-        parsed_input.append(["END", tag, []])
-
 
 parser = HTMLToAsciiDoc()
 with open("input.html", "r") as file:
     input = file.read()
 parser.feed(input)
 
-for loop_itr in parsed_input:
-    print(loop_itr)
+list_dependencies = []
+list_dependency_usages = []
+iter_out = iterative(parsed_input)
 
-out = iterative(parsed_input)
-print("iter: output: ", out)
+count = 0
+for item in iter_out:
+    if not any(item[1] in sl for sl in list_dependency_usages):
+        list_dependencies.append(item[1])
+        list_dependency_usages.append([item[1], []])
+    count = count + 1
+
+for item in iter_out:
+    pos = list_dependencies.index(item[1])
+    if not item[1] in list_dependency_usages:
+        list_dependency_usages[pos][1].append(item[0])
+
+
+print(list_dependency_usages)
