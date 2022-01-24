b819c9c8826e49e2003340e5060b121b0c8cf701
bitfl0wer <fleeprivate@gmail.com>
2022-01-24 16:22:31 +0100

    Backup

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from html.parser import HTMLParser
 
+# IDENTIFIER, CONTENT, CHILDREN[]
+parsed_input = []
+
+
+def iterative(data):
+    for data_iter in data:
+        if data[data_iter][1] == 'file' or data[data_iter][1] == 'dependency':
+            return
+
 
 class HTMLToAsciiDoc(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print("Start tag encountered: " + tag)
+        children = []
+        for attr in attrs:
+            children.append(["ATTR", attr, []])
+        parsed_input.append(["START", tag, children])
 
     def handle_endtag(self, tag):
-        print("End tag encountered: " + tag)
-
-    def handle_data(self, data):
-        print("Data encountered: " + data)
+        parsed_input.append(["END", tag, []])
 
 
 parser = HTMLToAsciiDoc()
+with open("input.html", "r") as file:
+    input = file.read()
+parser.feed(input)
+for loop_itr in parsed_input:
+    print(loop_itr)
